24f6169aec830c75e00e0e343e1af73ba01c311d
SarafNusrat <saraf.nusrat11@gmail.com>
2022-03-26 21:42:46 +0600

    all data added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect( () => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    }, [])
+
     return (
-        <div>
-            
+        <div className='shop-container'>
+           <div className="products-container">
+                <h1>This is produc</h1>
+           </div>
+           <div className="cart-container">
+                <h1>Order</h1>
+           </div>
         </div>
     );
 };
